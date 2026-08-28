ea05305c71da224fd2e6e6ca7c5bce967af39afe
KingDude061 <81912258+KingDude061@users.noreply.github.com>
2026-08-28 01:29:41 -0400

    pid should be done im so tired.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_prolly_BLUE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_prolly_BLUE.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.core.math.MathUtils;
+
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
@@ -11,31 +13,65 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.opencv.core.Mat;
 
 @Autonomous
 public class Auto_prolly_BLUE extends LinearOpMode {
     private void setPowers(double FL, double BL, double FR, double BR ){
-        FLMotor.setPower(FL);
-        BLMotor.setPower(BL);
-        FRMotor.setPower(FR);
-        BRMotor.setPower(BR);
+        FLMotor.setPower(Math.max(-1,Math.min(FL,1)));
+        BLMotor.setPower(Math.max(-1,Math.min(BL,1)));
+        FRMotor.setPower(Math.max(-1,Math.min(FR,1)));
+        BRMotor.setPower(Math.max(-1,Math.min(BR,1)));
     }
-    private void DriveTo(DistanceUnit sigma, double x, double y){
-        //is getting slope i need this in radians tho
-        double desiredHeading= (y-pinpoint.getPosX(sigma))/(x-pinpoint.getPosX(sigma));
-        double roboYaw = -(pinpoint.getHeading(AngleUnit.RADIANS)+desiredHeading);
-        double xPower =  -Math.sin(roboYaw);
-        double yPower =   Math.cos(roboYaw);
-        double stickTotal = Math.max(xPower,Math.max(yPower,1));
+    private void driveTo(DistanceUnit sigma, double x, double y){
+        //dt is a tiny amount of times, it's a mystery tool that will help us later.
+        //(cool calculus kids know what's up)
+        double dt;
 
-        double FLMotorPower = ((yPower + xPower) / stickTotal);
-        double FRMotorPower = ((yPower - xPower) / stickTotal);
-        double BLMotorPower = ((yPower - xPower) / stickTotal);
-        double BRMotorPower = ((yPower + xPower) / stickTotal);
+        //Some more mystery tools that will help us later
+        double integralX = 0;
+        double integralY = 0;
+        double readingIntegralX;
+        double readingIntegralY;
+        double derivativeX;
+        double derivativeY;
+        double proportionalX;
+        double proportionalY;
 
-        while(pinpoint.getPosX(sigma)!=x&&pinpoint.getPosY(sigma)!=y){
+        pinpoint.update();
+        while(opModeIsActive()&&Math.sqrt(Math.pow(x-pinpoint.getPosX(sigma),2)+Math.pow(y-pinpoint.getPosY(sigma),2))>1){
+            double previousX=pinpoint.getPosX(sigma);
+            double previousY=pinpoint.getPosY(sigma);
+            double timeSinceLastUpdate=System.currentTimeMillis();
+            pinpoint.update();
+            double desiredHeading=Math.atan2((y-pinpoint.getPosY(sigma)),(x-pinpoint.getPosX(sigma)));
+            //where i should tell the robot were pointing so we can go where i want to go
+            double roboYaw = -(pinpoint.getHeading(AngleUnit.RADIANS)+desiredHeading);
+            //This got a lil complicated but essentially its just a vector
+            double errorX = x-pinpoint.getPosX(sigma);
+            double errorY = y-pinpoint.getPosY(sigma);
+            double previousErrorX = x-previousX;
+            double previousErrorY = y-previousY;
+            dt=Math.max(0.0001,System.currentTimeMillis()-timeSinceLastUpdate)/1000;
+            integralX+=errorX*dt;
+            integralY+=errorY*dt;
+            //dont want to change the actual integral, bc that would mess up calculations
+            // so we make a new variable and multiply that one by the KI
+            readingIntegralX=integralX*KI;
+            readingIntegralY=integralY*KI;
+            derivativeX=KD*((errorX-previousErrorX)/dt);
+            derivativeY=KD*((errorY-previousErrorY)/dt);
+            proportionalX=errorX*KP;
+            proportionalY=errorY*KP;
 
+            double OutputX = 1.1*(readingIntegralX+derivativeX+proportionalX);
+            double OutputY = -(readingIntegralY+derivativeY+proportionalY);
+
+            double xPower = OutputX * Math.cos(roboYaw) + OutputY * Math.sin(roboYaw);
+            double yPower = OutputY * Math.cos(roboYaw) - OutputX * Math.sin(roboYaw);
+            setPowers((yPower+xPower),(yPower-xPower),(yPower-xPower),(yPower+xPower));
         }
+        setPowers(0,0,0,0);
     }
     private DcMotor Intake = null;
     private DcMotor Transfer = null;
@@ -46,6 +82,12 @@ public class Auto_prolly_BLUE extends LinearOpMode {
     private HuskyLens huskyLens = null;
     private Limelight3A limelight = null;
     private GoBildaPinpointDriver pinpoint;
+
+    //PID BS
+    //needs testing
+    private final double KP = 0.0125;
+    private final double KD = 0;
+    private final double KI = 0;
     @Override
     public void runOpMode() throws InterruptedException {
         //Start by initallizing all the cameras, motors, and also the pinpoint
@@ -94,7 +136,7 @@ public class Auto_prolly_BLUE extends LinearOpMode {
 
         //run this code once
         if (opModeIsActive()){
-            DriveTo(DistanceUnit.INCH,67,67);
+            driveTo(DistanceUnit.INCH,67,67);
 
         }
 
